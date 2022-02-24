9de1a5682e208ad0d67bec08904456d835e83339
TYama <alohaty81@gmail.com>
2022-02-23 17:53:00 -0700

    coding challenge done

---
+++ printDigits.js
@@ -0,0 +1,11 @@
+
+let printDigits = num => {
+    while(num > 0){
+        let lastDigit = num%10;
+        console.log(lastDigit);
+        num = Math.floor(num / 10);
+    }
+}
+
+printDigits(923);
+
