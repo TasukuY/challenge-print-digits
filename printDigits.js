979b5686cb665695365f96b611fdd90af1dcda75
TYama <alohaty81@gmail.com>
2022-02-24 10:26:06 -0700

    added other solutions

--- printDigits.js
+++ printDigits.js
@@ -4,6 +4,8 @@ let printDigits = num => {
         let lastDigit = num%10;
         console.log(lastDigit);
         num = Math.floor(num / 10);
+        //Another way to do a line above
+        //num = (num - remainder) / 10.
     }
 }
 
